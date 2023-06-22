2748e60d1bf3a8fa06940879bbcdba2af231d5f5
Ibuki <64850924+eve00@users.noreply.github.com>
2023-06-22 17:20:13 +0900

    entityを実装 (#3)

---
+++ review.php
@@ -0,0 +1,18 @@
+<?php
+
+class Review{
+  public $_subjectId;
+  function __construct($id){
+    $this->_subjectId = $id;
+  }
+  private $_text = "";
+
+  public function getText()
+  {
+    return $this->_text;
+  }
+  public function setText($text)
+  {
+    $this->_text = $text;
+  }
+}
--- student.php
+++ student.php
@@ -1,21 +1,37 @@
 <?php
-header('content-type: application/json; charset=utf-8');
 
-class Student {
-  public $subjects = array("科目1", "科目2", "科目3");
-  public function vote() {
-    return array('vote' => "dummy_string");
+class Student
+{
+  public $studentNo = 1;
+  public $subjects = array(1, 2, 3, 4, 5);
+  public $revs = array_map(function ($revId) {
+    return new Review($revId);
+  }, $revIds);
+
+  function __construct()
+  {
+    print_r($this->revs);
+  }
+
+  public function subjects()
+  {
+    /* TODO: subjectのリストを返す  */
+    return $this->subjects;
+  }
+
+  public function reviews($revId)
+  {
+    return new Review($revId);
+  }
+
+  public function getReviewText($id)
+  {
+    return $this->revs[$id]->getText();
+  }
+
+  public function setReviewText($id, $text)
+  {
+    /* TODO: レビューテキストをsetする */
+    $this->revs[$id]->setText($text);
   }
 }
-
-$st = new Student();
-
-if($_GET['method'] === "subjects"){
-  echo json_encode($st->subjects);
-}else if($_GET['method'] === "vote"){
-  echo json_encode($st->vote());
-}else{
-  echo json_encode(array('error'=>"unknown_method"));
-}
-exit();
-?>
