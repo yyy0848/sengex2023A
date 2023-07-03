594ac49bbb1d78559d29c93dafd448cf0aaf5704
Ibuki <64850924+eve00@users.noreply.github.com>
2023-07-03 20:43:58 +0900

    Show list (#6)
    * 実装(仮)
    
    * 修正
    
    * メソッド名変更
    
    * entity修正
    
    * getTitle修正
    
    * subject修正
    
    * student修正
    
    * とりあえず動く
    
    * タイトル一覧表示
    
    * showListに移植する
    
    * ページにリストを出力

--- subject.php
+++ subject.php
@@ -1,12 +1,25 @@
 <?php
 
-class Subject{
-
-  public $data = array(1 => '科目１', 2 => '科目２', 3 => '科目３');
-
+class Subject
+{
   public function getTitle($id)
   {
-      return $this->data[$id];
+    $title = $this->getDummyData($id);
+    return $title;
+  }
+
+  private function getDummyData($id)
+  {
+    $data = array(1 => 'インターネット技術', 2 => '情報システム基盤技術', 3 => 'ウェブデザイン演習', 4 => 'プログラミング言語', 5 => 'HCIデザイン',);
+    return $data[$id];
   }
 }
 
+$subject = new Subject();
+if (isset($_GET["method"])) {
+  if ($_GET['method'] === "getTitle") {
+    echo json_encode($subject->getTitle($_GET["id"]));
+  } else {
+//　echo json_encode(array("error" => "unknown_method"));
+  }
+}
