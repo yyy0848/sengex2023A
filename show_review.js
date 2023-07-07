afc978026dcd2c1db35cbd27d82ead7a4df71f60
yyy0848 <yyy0848@users.noreply.github.com>
2023-07-07 22:56:53 +0900

    show()は作動しているが、画面には反映されていない状態

--- show_review.js
+++ show_review.js
@@ -6,7 +6,6 @@ function ShowReview() {}
 ShowReview.prototype.showReview = function() {
 };
 
-
 const editEl = document.getElementById('edit') ?? null;
 if (editEl)
   editEl.addEventListener('click', function () {
@@ -21,20 +20,21 @@ if (closeEl)
     showList();
   });
 
+show(4); 
+//show(subjectId)自体は上手く作動している
 function show(subjectId) {
-  console.log("ここまではいってる");
   
   /* TODO:  取得したタイトル、レビューテキストをshowReviewページの要素内にappendする*/
-  /* jsonは配列（Array）なので、直接append(json)するのはできない */
+  /* jsonは配列（Array）なので、直接append(json)するのはできない→JSON.stringify(json)で解決 */
   $.getJSON("subject.php", { method: "getTitle", id: subjectId }, function (json) {
     console.log(json);
-    $("#title").append("<h2> レビュー：" + json_title + " </h2>");
+    $("#title").append("<h2> レビュー：" + JSON.stringify(json) + " </h2>");
     //$('#title').append(json);
   });
 
   $.getJSON("student.php", { method: "getReviewText", id: subjectId }, function (json) {
-    console.log(json);
-    $("#title").append("<textarea readonly row='" + 4 + "' col='" + 40 + "''>" + json_title + " </textarea>");
+    console.log("review:" + json);
+    $("#title").append("<textarea readonly row='" + 4 + "' col='" + 40 + "''>" + json + " </textarea>");
     //$('#title').append(json);
   });
 }
--- subject_list.js
+++ subject_list.js
@@ -10,11 +10,12 @@ SubjectList.prototype.subjectList = function () {
 
 const showEl = Array.from(document.getElementsByClassName('reviewed')) ?? null
 if (showEl)
+  //console.log(document.getElementById('reviewed'));
   showEl.forEach(function (target) {
     target.addEventListener('click', function () {
       location = './ShowReview.html'
       /* TODO:遷移先の科目のIDを動的に渡す */
-      showReview(document.getElementById('reviewed'));
+      showReview(5);
     });
   });
 
@@ -24,7 +25,7 @@ if (newReviewEl)
     target.addEventListener('click', function () {
       location = './EditReview.html'
       /* TODO:遷移先の科目のIDを動的に渡す */
-      newReview(document.getElementById('noReview'));
+      newReview(5);
     });
   });
 
@@ -33,7 +34,7 @@ if (newReviewEl)
 function showList() {
   //$("#subjects").append("<li class='noReview' id='"+1+"'>" + "test" + " </a> </li>");
   $.getJSON("student.php", { method: "subjects" }, function (json_id) {
-    console.log("json_id: " + json_id);
+    //console.log("json_id: " + json_id);
     for (let id of json_id)
       $.getJSON("subject.php", { method: "getTitle", id: id }, function (json_title) {
         $("#subjects").append("<li> <a href='#' class='reviewed' id='"+id+"'>" + json_title + " </a></li>");
@@ -42,6 +43,7 @@ function showList() {
 }
 
 function showReview(id) {
+  //console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + id);
   show(id);
 
 }
