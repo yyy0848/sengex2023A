79d53e664521b40ae011717354ecd20839065c9a
eve00 <64850924+eve00@users.noreply.github.com>
2023-07-10 09:21:23 +0900

    科目リストにレビュー状態を表示する

--- subject_list.js
+++ subject_list.js
@@ -9,47 +9,50 @@ SubjectList.prototype.subjectList = function () {
 function showList() {
   $.getJSON("student.php", { method: "subjects" }, function (json_id) {
     var parentEl = document.getElementById('subjects');
-    for (let id of json_id) {
-      $.getJSON("subject.php", { method: "getTitle", id: id }, function (json_title) {
-        var childEl = document.createElement('li');
-        childEl.id = id;
-        childEl.textContent = json_title;
-        parentEl.appendChild(childEl);
-        childEl.addEventListener("click", function () {
-
-          $.getJSON("student.php", { method: "getReviewText", id: id }, function (json_review) {
-            console.log("review:" + json_review);
-            $("#title").append("<textarea readonly row='" + 4 + "' col='" + 40 + "''>" + json_review + " </textarea>");
-            /*使う関数を動的に指定する（showReviewとnewReview）*/
-          if (json_review === "") {
-            newReview(id);
-          } else {
-            showReview(id);
-          }
-          });
-        });
-
-        });
-        /* getJSONでネストするとうまく行かないぽい */
-        // $.getJSON("student.php", { method: "getReviewText", id: id }, function (text) {
-        //   console.log(text === "");
-        //   var reviewState = (text === "") ? ("noReview") : ("reviewed");
-        //   console.log(reviewState);
-        //   childEl.class = reviewState;
-        //   childEl.addEventListener("click", function () {
-        //     if (reviewState) {
-        //       newReview(id);
-        //     } else {
-        //       showReview(id);
-        //     }
-        //     /* 遷移先はここで決まる */
-        //   });
-        // });
-
-
-      }
+    iterateIds(json_id, parentEl);
   });
-  console.log(document);
+}
+function iterateIds(ids, parentEl) {
+  if (ids.length === 0) {
+    // 全てのIDを処理した場合、終了する
+    return;
+  }
+  var id = ids.shift();
+  console.log(0);
+  $.getJSON("subject.php", { method: "getTitle", id: id }, function (json_title) {
+    console.log(1);
+    var childEl = document.createElement('li');
+    childEl.id = id;
+    childEl.textContent = json_title;
+    parentEl.appendChild(childEl);
+    console.log(childEl);
+    childEl.addEventListener("click", function () {
+      $.getJSON("student.php", { method: "getReviewText", id: id }, function (json_review) {
+        $("#title").append("<textarea readonly row='" + 4 + "' col='" + 40 + "''>" + json_review + " </textarea>");
+        /*使う関数を動的に指定する（showReviewとnewReview）*/
+        if (json_review === "") {
+          newReview(id);
+          //console.log("review:" + json_review);
+        } else {
+          showReview(id);
+        }
+      });
+    });
+    parentEl.appendChild(childEl);
+    //レビュー状態要素を追加
+    $.getJSON("student.php", { method: "getReviewText", id: id }, function (text) {
+      if (text !== "") {
+        var reviewStateEl = document.createElement('div');
+        reviewStateEl.className = "reviewed";
+        reviewStateEl.textContent = "レビューあり";
+        parentEl.append(reviewStateEl);
+      }
+    });
+
+
+    // 次のIDを処理するために再帰的に呼び出す
+    iterateIds(ids, parentEl);
+  });
 }
 
 
@@ -66,8 +69,8 @@ function newReview(id) {
 
 $(function () {
   const sl = new SubjectList();
-
   sl.subjectList();
 });
+
 
 export { showList };
